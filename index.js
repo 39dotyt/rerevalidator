beb3421d7d89645ca7693b55d69fc56461649c96
Yurij Mikhalevich <0@39.yt>
2014-01-24 18:28:12 +0400

    Migrated to script-server

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  * @author 0@39.yt (Yurij Mikhalevich)
  */
 
-var path = require('path'),
-    uglifyjs = require('uglify-js');
+var scriptServer = require('script-server');
 
 
 /**
@@ -18,23 +17,9 @@ exports.validator = require('./rerevalidator');
  * @param {Object} srv
  */
 exports.serve = function(pathToFiltersJs, srv) {
-  var url = '/rerevalidator/minified.js',
-      evs = srv.listeners('request').slice(0);
-  srv.removeAllListeners('request');
-  srv.on('request', function(req, res) {
-    if (0 === req.url.indexOf(url)) {
-      var result = uglifyjs.minify([
-        path.join(__dirname, '/node_modules/revalidator/lib/revalidator.js'),
-        path.join(__dirname, '/rerevalidator.js'),
-        pathToFiltersJs
-      ]);
-      res.writeHead(200, {'Content-Type': 'application/javascript'});
-      res.write(result.code);
-      res.end();
-    } else {
-      for (var i = 0; i < evs.length; i++) {
-        evs[i].call(srv, req, res);
-      }
-    }
-  });
+  scriptServer.serve('/rerevalidator/minified.js', [
+    path.join(__dirname, '/node_modules/revalidator/lib/revalidator.js'),
+    path.join(__dirname, '/rerevalidator.js'),
+    pathToFiltersJs
+  ], srv);
 };
